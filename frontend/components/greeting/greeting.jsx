a1ef6040fb81809698bcad3f3626010b377e9f91
alissara <alissara.r@gmail.com>
2017-05-17 00:02:08 -0700

    Add modals to signup/login links

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,23 +1,93 @@
 import React from 'react';
+import Modal from 'react-modal';
 import { Link } from 'react-router-dom';
+import style from './modal_style';
 
-const sessionLinks = () => (
-  <nav className="login-signup">
-    <Link to="/login">Login</Link>
-    <Link to="/signup">Sign up!</Link>
-  </nav>
-);
+import SessionFormContainer from '../session_form/session_form_container';
+import { AuthRoute } from '../../util/route_util';
 
-const personalGreeting = (currentUser, logout) => (
-	<nav className="header-right">
-    <h2 className="header-name">Hi, {currentUser.username}!</h2>
-    <button className="header-button" onClick={logout}>Log Out</button>
-    <button className="header-upload">Upload</button>
-	</nav>
-);
+// const sessionLinks = () => (
+//   <nav className="login-signup">
+//     <Link to="/login" onClick={this.handleClick}>Login</Link>
+//     <Link to="/signup" onClick={this.handleClick}>Sign up!</Link>
+//
+//     <Modal
+//       isOpen={this.state.modalOpen}>
+//       ...content
+//     </Modal>
+//   </nav>
+// );
 
-const Greeting = ({ currentUser, logout }) => (
-  currentUser ? personalGreeting(currentUser, logout) : sessionLinks()
-);
+// const personalGreeting = (currentUser, logout) => (
+// 	<nav className="header-right">
+//     <Link to="#" className="header-name">Hi, {currentUser.username}!</Link>
+//     <Link to="#" className="header-button" onClick={logout}>Log Out</Link>
+//     <Link to="#" className="header-upload">Upload</Link>
+// 	</nav>
+// );
+
+// const Greeting = ({ currentUser, logout }) => (
+//   currentUser ? personalGreeting(currentUser, logout) : sessionLinks()
+// );
+
+class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpen: false,
+      signIn: false
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.onModalClose = this.onModalClose.bind(this);
+    this.onModalOpen = this.onModalOpen.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ modalOpen: true});
+  }
+
+  onModalClose() {
+    this.setState({ modalOpen: false });
+    style.content.opacity = 0;
+  }
+
+  onModalOpen() {
+    style.content.opacity = 100;
+  }
+
+  render () {
+    if (this.props.currentUser) {
+      return (
+        <nav className="header-right">
+          <Link to="#" className="header-name">Hi, {this.props.currentUser.username}!</Link>
+          <Link to="#" className="header-button" onClick={this.props.logout}>Log Out</Link>
+          <Link to="#" className="header-upload">Upload</Link>
+        </nav>
+      );
+    } else {
+      return (
+        <nav className="login-signup">
+          <Link to="/login" onClick={this.handleClick}>Login</Link>
+          <Link to="/signup" onClick={this.handleClick}>Sign up!</Link>
+
+          <Modal
+            contentLabel="Modal"
+            isOpen={this.state.modalOpen}
+            onRequestClose={this.onModalClose}
+            style={style}
+            onAfterOpen={this.onModalOpen}
+          >
+            <button onClick={this.onModalClose}>X</button>
+              <AuthRoute path="/login" component={SessionFormContainer} />
+              <AuthRoute path="/signup" component={SessionFormContainer} />
+              AuthForm
+          </Modal>
+        </nav>
+      );
+    }
+  }
+
+}
 
 export default Greeting;
--- frontend/hellapx.jsx
+++ frontend/hellapx.jsx
@@ -1,21 +1,30 @@
 //React
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 //Components
 import configureStore from './store/store';
 import Root from './components/root';
 
 //Test
-import * as APIUtil from './util/session_api_util';
-import { login, logout, signup } from './actions/session_actions';
-window.store = configureStore();
-window.APIUtil = APIUtil;
-window.login = login;
-window.logout = logout;
-window.signup = signup;
+// import * as APIUtil from './util/session_api_util';
+// import { login, logout, signup } from './actions/session_actions';
+// window.store = configureStore();
+// window.APIUtil = APIUtil;
+// window.login = login;
+// window.logout = logout;
+// window.signup = signup;
 
 document.addEventListener('DOMContentLoaded', () => {
-  let store = configureStore();
+  Modal.setAppElement(document.body);
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
 });
